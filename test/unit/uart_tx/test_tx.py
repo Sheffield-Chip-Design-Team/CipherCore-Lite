05579b36b9a7f07a80de56305ad0e8266c7d3252
James <162901067+Enjimneering@users.noreply.github.com>
2025-12-10 01:05:06 +0000

    Create tx_test

--- test/unit/uart_tx/test_tx.py
+++ test/unit/uart_tx/test_tx.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
 # SPDX-License-Identifier: Apache-2.0
 
+import logging
 import cocotb
 import random
 from cocotb.clock import Clock
@@ -16,12 +17,42 @@ async def reset_dut(dut):
     await ClockCycles(dut.clk, 5)
     dut._log.info("DUT Reset complete")
 
+async def trigger_byte_frame(dut, data=256):
+    if data >= 256:
+        data = random.randint(0, 255)
+       
+    data_byte = data & 0xFF 
+    dut._log.info(f"Requesting DUT to send byte: {format(hex(data_byte))}")
+
+    dut.tx_data.value = data_byte
+    await ClockCycles(dut.clk, 1)
+    dut.tx_start.value = 1
+    await ClockCycles(dut.clk, 1)
+    dut.tx_start.value = 0
+
+    return data_byte
+
+def calc_parity(data_byte, parity_bit, parity_type='odd'):
+    """ Check odd parity for the given data byte """
+    calculated_parity = 0
+    for i in range(8):
+        calculated_parity ^= (data_byte >> i) & 0x1
+
+    if parity_type == 'odd':
+        calculated_parity ^= 0x1  # Invert for odd parity
+
+    return calculated_parity
+
 @cocotb.test()
 async def test_dut_tx(dut):
+    
+    num_tests = 100
     
     # Connect to tx UART VIP
     # Automatically set to 9600 baud and odd parity
     tx_Uart = UART.UartVIP(dut, dut_tx_pin="tx")
+    tx_Uart.is_active = False  # Set to monitor mode
+    tx_Uart.log.setLevel(logging.INFO)
 
     # Set the clock period to 1042 ns 
     dut._log.info("Starting clock")
@@ -30,26 +61,19 @@ async def test_dut_tx(dut):
 
     # Reset DUT
     await reset_dut(dut)
-    
+
     # Test UART TX by sending random bytes from DUT and monitoring them with the VIP
-    for i in range(10):
-        data_byte = random.randint(0, 255)
-        dut._log.info(f"Requesting DUT to send byte: {format(hex(data_byte))}")
+    for i in range(num_tests):
         
-        dut.tx_data.value  = data_byte
-        await ClockCycles(dut.clk, 1)
-        dut.tx_start.value = 1
-        read_byte = await tx_Uart.serial_read_byte()
+        await Timer(random.randint(10, 500), unit='us')  # Random delay between sending frames
+        
+        data_byte            = await trigger_byte_frame(dut)
+        monitored_uart_trans = await tx_Uart.serial_read_byte()
 
-        # byte = random.randint(0, 255)
-        # await tx_Uart.serial_write_byte(byte)
-        # tx_Uart.log.info(f"Sent Byte {format(hex(byte))} to DUT rx bus.")
-       
-        # await ClockCycles(dut.CLK, random.randint(10, 50))  # Wait for some cycles to before checking
-        
-        # assert dut.RX_VALID.value == 1, "RX_DONE not asserted"
-        # assert dut.RX_DATA.value.to_unsigned() == byte, f"RX Data Mismatch: Expected {format(hex(byte))}, Got {format(hex(dut.RX_DATA.value.integer))}"
-        
-        # dut._log.info(f"Byte: {format(hex(byte))} received correctly!")
+        # Check received transaction
+        assert monitored_uart_trans.parity_bit == calc_parity(data_byte, monitored_uart_trans.parity_bit), "Incorrect Parity Bit Detected"
+        assert monitored_uart_trans.data == data_byte, f"TX Data Mismatch: Expected {format(hex(data_byte))}"
+        assert monitored_uart_trans.start_bit == 0, "High Start Bit Detected"
+        assert monitored_uart_trans.stop_bit == 1, "Low Stop Bit Detected"
 
-        # await Timer(random.randint(1, 10000), unit='us')  # Random delay between packets
+        dut._log.info(f"Scoreboard Correctly Received Byte: {format(hex(data_byte))}. Test {i+1}/{num_tests} Passed.")
