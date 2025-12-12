b53034a109ac94c90452e9945e75bc23a3202ad2
James <162901067+Enjimneering@users.noreply.github.com>
2025-12-12 07:58:44 +0000

    add encryption functionality in cctop

--- src/cc_fpga_top.v
+++ src/cc_fpga_top.v
@@ -7,11 +7,12 @@ module CipherCore_FPGA_Top (
     input wire         clk,             
     input wire         rst_n,
     input wire         rx_in,
-    output wire  [7:0] rx_data,
+    output reg  [7:0]  rx_data,
     output wire        done,
     output wire        valid
 );
     wire clk_30MHz;
+    wire  [7:0] rx_data_plain;
     
     clk_div div
     (
@@ -25,11 +26,13 @@ module CipherCore_FPGA_Top (
       .clk(clk_30MHz),
       .rst_n(rst_n),
       .rx_in(rx_in),
-      .rx_data(rx_data),
+      .rx_data(rx_data_plain),
       .done(done),
       .valid(valid)
   );
   
+  // ADD encryption code here! 
+  rx_data = rx_data_plain; 
   
   
 
