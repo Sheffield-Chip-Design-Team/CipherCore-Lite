d8d8d9d5a6b0c430148e7492ff3feba68e0069d6
James <162901067+Enjimneering@users.noreply.github.com>
2025-12-16 22:02:11 +0000

    integrate tx and rx

--- src/cc_fpga_top.v
+++ src/cc_fpga_top.v
@@ -4,36 +4,40 @@
  */
 
 module CipherCore_FPGA_Top (
-    input wire         clk,             
-    input wire         rst_n,
-    input wire         rx_in,
-    output reg  [7:0]  rx_data,
-    output wire        done,
-    output wire        valid
+
+    input  wire         clk,             
+    input  wire         rst_n,
+    // rx interface
+    input  wire         rx_in,
+    output wire  [7:0]  rx_data,
+    output wire         rx_done,
+    output wire         rx_valid,
+    // tx interface
+    output wire          tx_out,
+    output wire         tx_done,
+    output wire         tx_busy
 );
+
     wire clk_30MHz;
-    wire  [7:0] rx_data_plain;
     
-    clk_div div
-    (
-      // Clock out ports
+    clk_div div (
       .clk_in(clk),         // input clk_in
       .resetn(1'b1),        // don't reset the clk div (synchronous reset)
-      .clk_out1(clk_30MHz)
+      .clk_out1(clk_30MHz)  // Clock out ports
     );
     
-  CipherCore_Top ciphercore_inst (
+    CipherCore_Top ciphercore_inst (
       .clk(clk_30MHz),
       .rst_n(rst_n),
+      // rx interface
       .rx_in(rx_in),
-      .rx_data(rx_data_plain),
-      .done(done),
-      .valid(valid)
-  );
-  
-  // ADD encryption code here! 
-  rx_data = rx_data_plain; 
-  
-  
+      .rx_data(rx_data),
+      .rx_done(rx_done),
+      .rx_valid(rx_valid),
+      // tx interface
+      .tx_out(tx_out),
+      .tx_busy(tx_busy),
+      .tx_done(tx_done)
+    );
 
 endmodule
--- src/ciphercore_top.v
+++ src/ciphercore_top.v
@@ -4,16 +4,23 @@
  */
 
 module CipherCore_Top (
-    input wire         clk,             
-    input wire         rst_n,
-    input wire         rx_in,
-    output wire  [7:0] rx_data,
-    output wire        done,
-    output wire        valid
-);
-    
-    wire rx_sync;
 
+    input  wire         clk,             
+    input  wire         rst_n,
+    // rx interface
+    input  wire         rx_in,
+    output wire  [7:0]  rx_data,
+    output wire         rx_done,
+    output wire         rx_valid,
+    // tx interface
+    output wire         tx_out,
+    output wire         tx_done,
+    output wire         tx_busy
+);
+
+   wire rx_sync;
+   reg old_rx_done;
+    
     synchronizer #(
         .STAGES(2)
     ) sync_rx_in (
@@ -31,8 +38,26 @@ module CipherCore_Top (
         .rst_n(rst_n),
         .rx(rx_sync),            
         .rx_data(rx_data),       
-        .done(done),          
-        .valid(valid)          
+        .done(rx_done),          
+        .valid(rx_valid)          
     );
+    
+    always @(posedge clk) begin
+      old_rx_done <= rx_done;
+    end
+ 
+    uart_tx #(
+        .CLK_FREQ(30_000_000),
+        .CYCLES_PER_BIT(3125)           
+    ) tx (
+        .clk(clk),
+        .rst_n(rst_n),
+        .tx_start(old_rx_done),
+        .tx_data(rx_data),       
+        .tx(tx_out),            
+        .busy(tx_busy),          
+        .done(tx_done)          
+    );
+
 
 endmodule
